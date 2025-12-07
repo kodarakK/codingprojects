098f47f7cb3cb84a61e913e00c5e67ca862f3d4e
Caradoc Roetynck <caradoc.roetynck@student.hogent.be>
2025-12-07 14:31:59 +0100

    day7p1

---
+++ aoc/2025/day7/p1.py
@@ -0,0 +1,26 @@
+with open("test_input.txt") as f:
+    data = f.readlines()
+
+count = 0
+for i in range(len(data)):
+    for j in range(len(data[0])-3):
+        if i == 0:
+            if data[i][j] == "S":
+                data[i+1] = data[i+1][:j] + "|" + data[i+1][j+1:]
+        if data[i][j] == "^" and data[i-1][j] == "|":
+            count += 1
+            data[i+1] = data[i+1][:j-1]+"|.|"+data[i+1][j+2:]
+        if i != len(data)-1 and data[i][j] == "|" and data[i+1][j] == ".":
+            data[i+1] = data[i+1][:j] + "|" + data[i+1][j+1:]
+            
+
+
+        
+
+for line in data:
+    print(line)
+print(count+1)
+
+with open ("output.txt", "w") as f:
+    for line in data:
+        f.write(line)
